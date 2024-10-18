86af7edb3e361d37715300d0e7544daea30de581
wand3 <godswillbaiyewu@gmail.com>
2024-10-18 18:00:56 +0100

    pages creation

---
+++ kasuwa-gaba/src/pages/Kasuwa.tsx
@@ -0,0 +1,20 @@
+import Jiki from "../components/Jiki";
+import NavButtom from "../components/navButtom";
+
+
+const Kasuwa = () => {
+
+    return (
+        <>
+            <body navButtom={true}>
+                <Jiki>
+
+                </Jiki>
+            
+            </body>
+            
+        </>
+    )
+}
+
+export default Kasuwa;
