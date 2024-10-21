85d0bfd2bf2563dc365b7b4171383aec0714c9f4
ezraliberty <ezraliberty99@gmail.com>
2024-10-21 07:05:20 +0100

    user table

--- kasuwa-baya/app/api/user.py
+++ kasuwa-baya/app/api/user.py
@@ -11,7 +11,7 @@ import logging
 def get_user(id):
     return db.get_or_404(User, id).to_dict()
 
-@bp.route('/signup', methods=['POST'])
+@bp.route('/user', methods=['POST'])
 def create_user():
     data = request.json()
     if 'name' not in data or 'username' not in data or 'email' not in data or 'password' not in data
@@ -29,3 +29,20 @@ def create_user():
     db.session.commit()
 
     return {'message': 'User Created Successfully', 'id': user.id}
+
+@bp.route('/edit_user/<int:id>', methods=['PUT'])
+def update_user(id):
+    user = db.get_or_404(User, id)
+    data = request.get_json()
+    if 'username' in data and data['username'] != user.username and \
+        db.session.scalar(select(User).where(
+            User.username == data['username'])):
+        return bad_request('please use a different username')
+    if 'email' in data and data['email'] != user.email and \
+        db.session.scalar(select(User).where(
+            User.email == data['email'])):
+        return bad_request('please use a different email address')
+
+    user.from_dict(data, new_user=False)
+    db.session.commit()
+    return user.to_dict()
