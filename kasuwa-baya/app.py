e584797657461e2d6d9094d96b2b7ba0a51f40d9
ezraliberty <ezraliberty99@gmail.com>
2024-10-27 19:19:22 +0100

    add

--- kasuwa-baya/app.py
+++ kasuwa-baya/app.py
@@ -13,3 +13,7 @@ app = create_app()
 @app.shell_context_processor
 def make_shell_context():
     return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Product': Product, 'Category': Category, 'BaseModel': BaseModel}
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Product': Product, 'Category': Category, 'BaseModel': BaseModel}
